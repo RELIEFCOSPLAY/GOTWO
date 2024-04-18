da718f1d53a7e09b716e4ef26d01d250ca82624c
RELIEFCOSPLAY <95217722+RELIEFCOSPLAY@users.noreply.github.com>
2024-04-18 22:41:52 +0700

    main update

---
+++ public/js/gotwo_js/payment_ride_nav_animation_completed.js
@@ -0,0 +1,34 @@
+// ------------------nav animation------------------
+let slider_nav_animation = document.querySelector(".slider_nav_animation");
+let slider_nav_animation_li = document.querySelectorAll(".nav_animation ul li");
+let index_value = 0;
+let left_position = 200;
+
+slider_nav_animation_li.forEach((item, index) => {
+  slider_nav_animation.style.width =
+    slider_nav_animation_li[0].clientWidth + "px";
+  slider_nav_animation.style.left = (left_position+30) + "px";
+
+  item.onclick = function () {
+    slider_nav_animation.style.width = item.clientWidth + "px";
+    console.log(index);
+    index_value = index;
+    get_left_position();
+    slider_nav_animation.style.left = (left_position+30) + "px";
+    left_position = 0;
+  };
+});
+// Getting left position for slider to slider
+function get_left_position() {
+  for (let i = 0; i < index_value; i++) {
+    const element = slider_nav_animation_li[i];
+    left_position += element.clientWidth;
+    console.log(left_position);
+  }
+}
+// ---------sidebar function---------
+const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("expand");
+});
