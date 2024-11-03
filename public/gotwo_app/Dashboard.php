31087e875f783c269c7f4c3b69ca69054342b2d9
merlin39 <6431501104@lamduan.mfu.ac.th>
2024-11-04 03:32:42 +0700

    นอนดีกว่าา

--- public/gotwo_app/Dashboard.php
+++ public/gotwo_app/Dashboard.php
@@ -8,7 +8,9 @@ $dbname = "gotwo";
 try {
     $conn = new PDO("mysql:host=$severname;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
 } catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
 
