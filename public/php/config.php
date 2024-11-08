e381ecee6f82c9082752ca212b54e4b7c945eed6
kuku112 <6431501112@lamduan.mfu.ac.th>
2024-11-08 22:08:33 +0700

    ..

---
+++ public/php/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$severname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gotwo";
+
+try {
+    $conn = new PDO("mysql:host=$severname;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
